ca72c959acb1034f54dd919b6a2a1541cd1d2b6c
Ocarinaoftime <89474990+Ocarinaoftime@users.noreply.github.com>
2023-05-17 16:15:37 -0400

    asd

--- main.js
+++ main.js
@@ -1,3 +1,33 @@
+import WIIMote from "./src/wiimote.js"
+
+
+
+requestButton.addEventListener("click", async () => {
+    let device;
+    try {
+      const devices = await navigator.hid.requestDevice({
+          filters: [{ vendorId: 0x057e }],
+      });
+      
+      device = devices[0];
+      wiimote = new WIIMote(device)
+  
+    } catch (error) {
+      console.log("An error occurred.", error);
+    }
+  
+    if (!device) {
+      console.log("No device was selected.");
+    } else {
+      console.log(`HID: ${device.productName}`);
+  
+      enableControls()
+      initCanvas()
+  
+    }
+  });
+
+
 window.onload = function() {
     setTimeout(() => {
         keyboardJS.pressKey('a')
